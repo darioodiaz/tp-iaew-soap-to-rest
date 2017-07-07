e316313c8b153870f61eb42ad6476d832f4e7d86
santiago <santiifonzalida@gmail.com>
2017-07-06 21:15:42 -0300

    consulta lista

--- frontend/src/app/components/reserva/reserva.component.ts
+++ frontend/src/app/components/reserva/reserva.component.ts
@@ -10,38 +10,46 @@ export class ReservaComponent implements OnInit {
 
   private token: string;
   public resultados: any[] = [];
-  paises:any=[];
-  ciudades:any=[];
-  public consulta:any = {};
+  paises: any = [];
+  ciudades: any = [];
+  public consulta: any = {};
 
-  constructor(private _activatedRoute:ActivatedRoute, private servicio: ReservaService) {
+  constructor(private _activatedRoute: ActivatedRoute, private servicio: ReservaService) {
 
-   }
+  }
 
   ngOnInit() {
     this.obtenerPaises();
   }
 
-  public obtenerPaises(){
-     this.servicio
-       .obtenerPaises()
-       .subscribe( resp => {
-          this.paises = resp;
-        });
+  public obtenerPaises() {
+    this.servicio
+      .obtenerPaises()
+      .subscribe(resp => {
+        this.paises = resp;
+      });
   }
 
-public obtenerCiudades(id:number){
-  this.servicio
-    .obtenerCiudades(id)
-    .subscribe( resp => {
-      this.ciudades = resp;
-    })
-}
+  public obtenerCiudades(id: number) {
+    this.servicio
+      .obtenerCiudades(id)
+      .subscribe(resp => {
+        this.ciudades = resp;
+      })
+  }
 
   public consultar() {
-    this.servicio.consultarVehiculos(this.consulta).subscribe( resp =>{
-      this.resultados = resp;
-    })
+    if (!this.consulta.length) {
+      this.servicio.consultarVehiculos(this.consulta).subscribe(resp => {
+        this.resultados = resp;
+      })
+    }
+
+
+  }
+
+  public reservarAuto() {
+    console.log("reservar automovil");
   }
 
 
